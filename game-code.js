6d6fbd648a9762e3e383e55535dadbfec13c8750
Natasha <natashasosar@gmail.com>
2022-11-17 20:27:27 -0300

    Adapt fuctions to display results and scores on graphic interface, stil need to fix game over function

--- game-code.js
+++ game-code.js
@@ -59,6 +59,40 @@ function getComputerChoice () {
     return computerChoice;
 }
 
+function displayPlayerChoice () {
+    const playerDisplay = document.querySelector('#imgPlayer');
+
+    playerDisplay.classList.remove('ocult');
+
+    if (playerChoice == 'rock') {       
+        playerDisplay.src = 'img/rock-player.png';
+    } else if (playerChoice == 'paper') {
+        playerDisplay.src = 'img/paper-player.png';
+    } else {
+        playerDisplay.src = 'img/scissors-player.png';
+    } 
+}
+
+function displayMachineChoice () {
+    const machineDisplay = document.querySelector('#imgMachine');
+
+    machineDisplay.classList.remove('ocult');
+
+    if (computerChoice == 'rock') {       
+        machineDisplay.src = 'img/rock-machine.png';
+    } else if (computerChoice == 'paper') {
+        machineDisplay.src = 'img/paper-machine.png';
+    } else {
+        machineDisplay.src = 'img/scissors-machine.png';
+    } 
+}
+
+function showScore () {
+    document.querySelector('#playerscore').innerHTML = ('YOU: ' + playerScore);
+    document.querySelector('#machinescore').innerHTML= ('MACHINE: ' + computerScore);
+}
+
+
 function playRound (playerChoice, computerChoice) {
 
     switch (true) {
@@ -66,25 +100,25 @@ function playRound (playerChoice, computerChoice) {
             result = ("ERROR!");
             break;
         case (playerChoice == "rock" && computerChoice == "scissors"):
-            result = ('You win! Rock beats Scissors!');
+            result = ('YOU WIN! ROCK BEATS SCISSORS!');
             break;
         case (playerChoice == "rock" && computerChoice == "paper"):
-            result = ('You lose! Paper beats Rock!');
+            result = ('YOU LOSE! PAPER BEATS ROCK!');
             break;
         case (playerChoice == "paper" && computerChoice == "rock"):
-            result =  ('You win! Paper beats Rock!');
+            result =  ('YOU WIN! PAPER BEATS ROCK!');
             break;   
         case (playerChoice == "paper" && computerChoice == "scissors"):
-            result =  ('You lose! Scissors beats Paper!');
+            result =  ('YOU LOSE! SCISSORS BEATS PAPER!');
             break;
         case (playerChoice == "scissors" && computerChoice == "paper"):
-            result = ('You win! Scissors beats Paper!');
+            result = ('YOU WIN! SCISSORS BEATS PAPER!');
             break;
         case (playerChoice == "scissors" && computerChoice == "rock"):
-            result =  ('You lose! Rock beats Scissors!');
+            result =  ('YOU LOSE! ROCK BEATS SCISSORS!');
             break;
         case (playerChoice === computerChoice):
-            result = ('It\'s a tie!');
+            result = ('IT\'S A TIE!');
             break;
         case (playerChoice !== "rock" || "paper" || "scissors"):
             result =  ('Oh-oh! Something went wrong, probably a typo. Try again :)');
@@ -94,22 +128,53 @@ function playRound (playerChoice, computerChoice) {
     return result;
 }
 
+function resultPhrase () {
+    document.querySelector('.subtitle').innerHTML = result;
+}
+
+// FIX THE FUCTION TO CHANGE THE PLACEHOLDER TEXT 
+function gameOver() {
+    document.location.href='game-over-page/index.html';
+    testfunction ();
+    const resultPhrase = document.getElementById('finalresult');
+    console.log (resultPhrase);
+    console.log ('olar');
+
+    if (playerScore == 5) {
+        document.querySelector('#finalresult').innerHTML = 'YOU BEAT THEM, YAY :)';
+        console.log ("Yay! You beat them, congrats :)");
+    } else {
+        document.querySelector('#finalresult').innerHTML = 'OH NO, YOU LOST :(';
+        console.log ("Oh no, you lost this time :(");
+    }
+}
+
+function testfunction () {
+    document.querySelector('.finalresult').innerHTML = 'FINGE QUE É UMA SEMENTE';
+}
+
+
 function game() {
-   /* while (computerScore < 5 && playerScore < 5) {
-        getPlayerChoice (); */
         getComputerChoice ();
         /* AQUI: FUNÇAO DE DISPLAY DA IMAGEM DO JOGADOR*/
+        displayPlayerChoice ();
         playRound (playerChoice, computerChoice);
         /* AQUI: FUNÇÃO DO DISPLAY DA IMAGEM DO COMPUTADOR */
-        if (computerScore == 5 || playerScore == 5) {
+        displayMachineChoice (); 
+        
+        if (computerScore == 4 || playerScore == 4) {
             gameOver();
-        } else if (result.includes('win')) {
+        } else if (result.includes('WIN')) {
             playerScore += 1;
-        } else if (result.includes('lose')){
+        } else if (result.includes('LOSE')){
             computerScore += 1;
-        }
+        };
+
         /*AQUI: FUNÇÃO QUE MOSTRA PONTOS*/
+        showScore ();
         /*AQUI FUNÇÃO QUE TROCA A FRASE DO TOPO PELO RESULTADO */
+        resultPhrase ();
+
         console.log ('You chose ' + playerChoice + '.');
         console.log ('Your opponent chose ' + computerChoice + '.');
         console.log (result);
@@ -121,13 +186,7 @@ function game() {
     return playerScore, computerScore;
 }
 
-function gameOver() {
-    if (playerScore == 5) {
-        console.log ("Yay! You beat them, congrats :)");
-    } else {
-        console.log ("Oh no, you lost this time :(");
-    }
-}
+
 
 
 /*console.log ("Let's begin. First player to score 5 points wins!");
